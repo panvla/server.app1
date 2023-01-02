c782879b07b303aeff4de78c12a92dda628fa2a9
panvla <pandvlado@gmail.com>
2023-01-02 12:19:45 +0100

    that's it

---
+++ server.app1/frondend/serverapp/src/app/app.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { map } from 'rxjs/internal/operators/map';
+import { startWith } from 'rxjs/internal/operators/startWith';
+import { catchError } from 'rxjs/operators';
+import { DataState } from './enum/data-state.enum';
+import { Status } from './enum/status.enum';
+import { AppState } from './interface/app-state';
+import { CustomResponse } from './interface/custom-response';
+import { ServerService } from './service/server.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+
+  appState$: Observable<AppState<CustomResponse>>;
+  readonly DataState = DataState;
+  readonly Status = Status;
+  private filterSubject = new BehaviorSubject<string>('');
+  private dataSubject = new BehaviorSubject<CustomResponse>(null);
+  filterStatus$ = this.filterSubject.asObservable();
+
+  constructor(private serverService: ServerService){}
+
+  ngOnInit(): void {
+    this.appState$ = this.serverService.servers$
+    .pipe(
+      map(response => {
+        this.dataSubject.next(response);
+        return { dataState: DataState.LOADED_STATE, appData: response}
+      }),
+      startWith({dataState: DataState.LOADING_STATE}),
+      catchError((error: string) => {
+        return of({dataState: DataState.ERROR_STATE, error: error})
+      })
+    );
+  }
+  
+  pingServer(ipAddress: string): void {
+    this.filterSubject.next(ipAddress);
+    this.appState$ = this.serverService.ping$(ipAddress)
+    .pipe(
+      map(response => {
+        this.dataSubject.value.data.servers[
+          this.dataSubject.value.data.servers.findIndex(server =>
+            server.id === response.data.server.id)] = response.data.server;
+            this.filterSubject.next('');
+            return { dataState: DataState.LOADED_STATE, appData: this.dataSubject.value}
+      }),
+      startWith({ dataState: DataState.LOADED_STATE, appData: this.dataSubject.value}),
+      catchError((error: string) => {
+        this.filterSubject.next('');
+        return of({dataState: DataState.ERROR_STATE, error})
+      })
+    );
+  }
+
+  filterServers(status: Status): void {
+    console.log("test test");
+    //const status = event as unknown as Status;
+    this.appState$ = this.serverService.filter$(status, this.dataSubject.value)
+    .pipe(
+      map(response => {
+        return { dataState: DataState.LOADED_STATE, appData: response}
+      }),
+      startWith({dataState: DataState.LOADED_STATE, appData: this.dataSubject.value}),
+      catchError((error: string) => {
+        return of({ dataState: DataState.ERROR_STATE, error});
+      })
+    );
+  }
+
+  saveServer(serverForm: NgForm): void {
+    this.appState$ = this.serverService.save$(serverForm.value as Server)
+    .pipe(
+      map(response => {
+        this.dataSubject.next(
+          {...response, data: {servers: [response.data.server, ...this.dataSubject.value.data.servers]}}
+        );
+        document.getElementById('closeModal').click();
+        serverForm.resetForm({status: this.Status.SERVER_DOWN});
+        return { dataState: DataState.LOADED_STATE, appData: this.dataSubject.value}
+      }),
+      startWith({dataState: DataState.LOADED_STATE, appData: this.dataSubject.value}),
+      catchError((error: string) => {
+        this.filterSubject.next('');
+        return of({dataState: DataState.ERROR_STATE, error});
+      })
+    );
+  }
+
+}
